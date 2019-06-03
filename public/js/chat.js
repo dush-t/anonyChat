945b5b261a191269abf99b94ed3b1eed1223e31f
Dushyant Yadav <dushyant9309@gmail.com>
2019-06-03 11:32:58 +0530

    Implement basic chat functionality

---
+++ public/js/chat.js
@@ -0,0 +1,69 @@
+const socket = io();
+
+// Elements
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#send-location');
+const $messages = document.querySelector('#messages');
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationMessageTemplate = document.querySelector('#location-template').innerHTML;
+
+
+
+socket.on('message', (message) => {
+    // console.log(message);
+    const html = Mustache.render(messageTemplate, {
+        message: message
+    });
+    $messages.insertAdjacentHTML('beforeend', html);
+})
+
+socket.on('locationMessage', (locationURL) => {
+    const html = Mustache.render(locationMessageTemplate, {
+        locationURL: locationURL
+    });
+    $messages.insertAdjacentHTML('beforeend', html);
+})
+
+
+
+$messageForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    $messageFormButton.setAttribute('disabled', 'disabled');
+
+    const message = e.target.elements.message.value;
+    socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled');
+        $messageFormInput.value = '';
+        $messageFormInput.focus();
+        if (error) {
+            return console.log(error);
+        }
+
+        console.log("Message delivered successfully");
+    });
+
+})
+
+$sendLocationButton.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser');
+    }
+
+    $sendLocationButton.setAttribute('disabled', 'disabled');
+
+    navigator.geolocation.getCurrentPosition((location) => {
+        console.log(location);
+        socket.emit('sendLocation', {
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude
+        }, () => {
+            $sendLocationButton.removeAttribute('disabled');
+            console.log("Location shared successful.")
+        });
+    })
+})  
